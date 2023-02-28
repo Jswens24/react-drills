a4905273eef586faba8b59214cdc6d2c707708d1
Jessica Swenson <jessicamswens@gmail.com>
2023-02-28 07:49:50 -0700

    added 9 router

--- app-9/src/App.js
+++ app-9/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, NavLink } from 'react-router-dom'
+import Home from './components/Home';
+import Signup from './components/Signup';
+import Details from './components/Details';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/signup'>Signup</NavLink>
+        <NavLink to='/details'>Details</NavLink>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/details' element={<Details />} />
+      </Routes>
     </div>
   );
 }
